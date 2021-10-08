dccc8fef85d577005d5b6303a781409ed56dc6fa
Nishant Giri <nishantgiri.20@gmail.com>
2021-10-08 16:39:05 +0530

    Update index.js
    Updated index.js for newly added categories.

--- index.js
+++ index.js
@@ -15,20 +15,47 @@ function getsound(txt)
         var audio = new Audio("sounds/hindi/"+randomnumber1+".mp3");
         audio.play();
         break;
+      
         case "English":
         var randomnumber2= Math.round(Math.random()*(5-1)+1);
         var audio = new Audio("sounds/english/"+randomnumber2+".mp3");
         audio.play();
-
         break;
+      
         case "Punjabi":
         var randomnumber3= Math.round(Math.random()*(5-1)+1);
         var audio = new Audio("sounds/punjabi/"+randomnumber3+".mp3");
         audio.play();
-
         break;
         
+        case "Classical":
+        var randomnumber4= Math.round(Math.random()*(2-1)+1);
+        var audio = new Audio("sounds/classical/"+randomnumber4+".mp3");
+        audio.play();
+        break;
 
-
+        case "Folk":
+        var randomnumber5= Math.round(Math.random()*(2-1)+1);
+        var audio = new Audio("sounds/classical/"+randomnumber5+".mp3");
+        audio.play();
+        break;
+      
+        case "Jazz":
+        var randomnumber6= Math.round(Math.random()*(2-1)+1);
+        var audio = new Audio("sounds/classical/"+randomnumber6+".mp3");
+        audio.play();
+        break;
+      
+        case "Pop":
+        var randomnumber7= Math.round(Math.random()*(2-1)+1);
+        var audio = new Audio("sounds/classical/"+randomnumber7+".mp3");
+        audio.play();
+        break;
+      
+        case "Rock":
+        var randomnumber8= Math.round(Math.random()*(2-1)+1);
+        var audio = new Audio("sounds/classical/"+randomnumber8+".mp3");
+        audio.play();
+        break;
   }
 }
