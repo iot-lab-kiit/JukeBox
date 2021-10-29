f63ab0556419ff81be8bc938b0d05fc1b151b84b
stonarini <stonarini@cifpfbmoll.eu>
2021-10-29 15:18:43 +0200

    add case for new categories in getsound

--- index.js
+++ index.js
@@ -24,29 +24,49 @@ function getsound(catagory) {
       aud.src = "sounds/punjabi/" + randomnumber3 + ".mp3";
       aud.play();
       break;
-    case "Classic":
+    case "Italian":
       var randomnumber4 = Math.round(Math.random() * 2) + 1;
-      aud.src = "sounds/classic/" + randomnumber4 + ".mp3";
+      aud.src = "sounds/italian/" + randomnumber4 + ".mp3";
+      aud.play();
+      break;
+    case "Classic":
+      var randomnumber5 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/classic/" + randomnumber5 + ".mp3";
       aud.play();
       break;
     case "Folk":
-      var randomnumber5 = Math.round(Math.random() * 2) + 1;
-      aud.src = "sounds/folk/" + randomnumber5 + ".mp3";
+      var randomnumber6 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/folk/" + randomnumber6 + ".mp3";
       aud.play();
       break;
     case "Jazz":
-      var randomnumber6 = Math.round(Math.random() * 2) + 1;
-      aud.src = "sounds/jazz/" + randomnumber6 + ".mp3";
+      var randomnumber7 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/jazz/" + randomnumber7 + ".mp3";
       aud.play();
       break;
     case "Pop":
-      var randomnumber7 = Math.round(Math.random() * 2) + 1;
-      aud.src = "sounds/pop/" + randomnumber7 + ".mp3";
+      var randomnumber8 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/pop/" + randomnumber8 + ".mp3";
       aud.play();
       break;
     case "Rock":
-      var randomnumber8 = Math.round(Math.random() * 2) + 1;
-      aud.src = "sounds/rock/" + randomnumber8 + ".mp3";
+      var randomnumber9 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/rock/" + randomnumber9 + ".mp3";
+      aud.play();
+      break;
+    case "Rap":
+      var randomnumber10 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/rap/" + randomnumber10 + ".mp3";
+      aud.play();
+      break;
+    case "Indie":
+      var randomnumber11 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/indie/" + randomnumber11 + ".mp3";
+      aud.play();
+      break;
+    case "Dubstep":
+      var randomnumber12 = Math.round(Math.random() * 2) + 1;
+      aud.src = "sounds/dubstep/" + randomnumber12 + ".mp3";
       aud.play();
       break;
   }
